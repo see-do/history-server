be8d04ab46d838950165cd04d83b2e8246902d59
sangho-jeon <jeonsangho1587@gmail.com>
2022-01-26 11:25:11 +0900

    [feat] docker image to ecr

--- src/main/java/com/UMC/history/controller/TestController.java
+++ src/main/java/com/UMC/history/controller/TestController.java
@@ -3,8 +3,6 @@ package com.UMC.history.controller;
 import com.UMC.history.entity.TestEntity;
 import com.UMC.history.repository.TestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
