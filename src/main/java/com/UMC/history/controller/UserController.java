76943ed995559ae342b9d6a2769acdc018b745f4
JoHyunho-clover <78728072+JoHyunho-clover@users.noreply.github.com>
2022-02-08 19:14:13 +0900

    import edit
    ResponseEntity를 제거하였습니다.

--- src/main/java/com/UMC/history/controller/UserController.java
+++ src/main/java/com/UMC/history/controller/UserController.java
@@ -6,7 +6,6 @@ import com.UMC.history.service.UserService;
 import com.UMC.history.util.CommonResponse;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
