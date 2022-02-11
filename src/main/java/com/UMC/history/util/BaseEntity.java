db337692258dd6294a328c38d6dc57a803ecff3f
chaerlo127 <monynony0203@gmail.com>
2022-02-11 16:14:10 +0900

    CREATE/MODIFY DATE GETTER

--- src/main/java/com/UMC/history/util/BaseEntity.java
+++ src/main/java/com/UMC/history/util/BaseEntity.java
@@ -1,5 +1,6 @@
 package com.UMC.history.util;
 
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -9,7 +10,7 @@ import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
-
+@Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
